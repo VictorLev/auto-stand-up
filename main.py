5e094553b224be7bf12a5720ebc4fb3a89ed3d53
Victor Lévesque <levesque.vic@gmail.com>
2024-05-11 14:34:43 +1000

    done

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ load_dotenv()
 
 # Initialize the client with your Notion API key
 notion = Client(auth=os.getenv("NOTION_KEY"))
+spandupPageId = os.getenv("STANDUP_PAGE_ID")
+archivePageId = os.getenv("ARCHIVE_PAGE_ID")
 today  = str(date.today())
 
 Week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
@@ -15,7 +17,7 @@ Week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 # Create a page
 page = notion.pages.create(
     parent={
-        "page_id": os.getenv("STANDUP_PAGE_ID")
+        "page_id": spandupPageId
     },
     properties={
         "title": {
@@ -37,8 +39,6 @@ page = notion.pages.create(
         }
     }
 )
-
-
 
 for day in Week:
     # Create Day Title
@@ -83,39 +83,29 @@ for day in Week:
             "object": "block",
             "type": "to_do",
             "to_do": {"rich_text": [], "checked": False}
+        },
+        {
+            "object": "block",
+            "type": "heading_3",
+            "heading_3": {
+                "rich_text": [
+                    {
+                        "type": "text",
+                        "text": {"content": "What Happened"}
+                    }
+                ]
+            }
+        },
+        {
+            "object": "block",
+            "type": "to_do",
+            "to_do": {"rich_text": [], "checked": False}
+        },
+        {
+            "object": "block",
+            "type": "to_do",
+            "to_do": {"rich_text": [], "checked": False}
         }
     ]
 
     notion.blocks.children.append(block_id=block_id, children=inner_block)
-
-# Create 2 Blocks
-
-# Append 2 Blcoks to the Day Title 
-
-
-# Adding headings and to-do tasks
-# Monday_block = [
-#     {
-#         "object": "block",
-
-#     },
-#     {
-#         "object": "block",
-#         "type": "heading_3",
-#         "heading_3": {
-#             "rich_text": [
-#                 {
-#                     "type": "text",
-#                     "text": {"content": "To Do"}
-#                 }
-#             ]
-#         }
-#     }
-# ]
-
-# USE append() FUNCTION TO CREATE EVERY WEEKBLOCK AND ADD THE CHILDREN BLOCKS
-# THEN ADD THE WEEKBLOCK TO THE PARENT PAGE
-# notion.blocks.children.append(block_id=page_id, children=Monday_block)
-
-
-
